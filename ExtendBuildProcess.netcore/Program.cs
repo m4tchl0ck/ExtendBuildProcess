8cf13ddfd3ef2cc3f0094de30afc90de6a15831d
Adrian <m4tchl0ck@outlook.com>
2020-03-01 22:48:37 +0100

    Init .net core project

---
+++ ExtendBuildProcess.netcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtendBuildProcess.netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
